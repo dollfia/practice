6ea9dcfa5d9bb253762eddbe328533605f85ec03
dollfia <dalfiafarish496@gmail.com>
2026-02-26 00:10:08 +0530

    add a details

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Name : Dalfia!" );
+        System.out.println( "Sex : Female" );
+        System.out.println("age : 23");
+        System.out.println( "height : 164" );
+        System.out.println( "weight : 64" );
+        System.out.println( "collage name :U.S.P arts and science collage" );
+        System.out.println( "dep : computer science" );
+        System.out.println( "current year : 2nd year" );
+        System.out.println( "current month : march" );
+        System.out.println( "current day : 31" );
+        System.out.println( "current hour : 12" );
+        System.out.println( "current minute : 59" );
+        System.out.println( "current second : 59" );
+        System.out.println( "current week : 7" );
+
     }
 }
