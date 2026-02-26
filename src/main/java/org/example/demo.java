24caf510e71f635aec843e2f5675eb087ac35093
dollfia <dalfiafarish496@gmail.com>
2026-02-26 17:38:55 +0530

    added new classes

---
+++ src/main/java/org/example/demo.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class demo {
+
+    public static void main( String[] args )
+    {
+        System.out.println( "collage name - u.s.p" );
+        System.out.println( "dep name - cs" );
+    }
+}
