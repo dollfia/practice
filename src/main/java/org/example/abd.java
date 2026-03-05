5b7b97dcdfed16b04bd54a1b4542c762023aa17e
dollfia <dalfiafarish496@gmail.com>
2026-03-06 01:30:28 +0530

    practice if else conditions

--- src/main/java/org/example/abd.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class abd {
-    public static void main( String[] args )
-    {
-        System.out.println( "<---- This has been created by Abd ----/>" );
-        System.out.println("this is the end");
-    }
-}
